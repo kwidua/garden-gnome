3806ce4106a4f1a4fe70ab9e1359c82573096186
kwidua <kwidua@gmail.com>
2026-03-17 11:27:28 +0100

    Add Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,27 @@
+import { Sprout } from "lucide-react";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+    const navigationButtons = "px-4 py-2 rounded-md transition-colors bg-primary-green text-white"
+
+    return (
+        <header className="sticky top-0 z-50 w-full">
+            <nav className="backdrop-blur-md shadow-sm mx-auto px-6 py-4">
+                <div className="flex items-center justify-between">
+                     <Link to="/my-plants" className="flex items-center gap-2">
+                        <div className="flex h-10 w-10 items-center justify-center rounded-full bg-primary-green">
+                        <Sprout className="h-6 w-6 text-white" />
+                        </div>
+                        <h1 className="text-lg sm:text-xl text-foreground">Garden Gnome</h1>
+                    </Link>
+
+                    <div className="flex gap-8">
+                    <Link to="/my-plants" className={navigationButtons}>My Plants</Link>
+                    <Link to="/calendar" className={navigationButtons}>Calendar</Link>
+                    <Link to="/todos" className={navigationButtons}>Todos</Link>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    )
+}
