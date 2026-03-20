df0fc952275138bb4882bff63e548f54b3c11404
kwidua <kwidua@gmail.com>
2026-03-20 09:50:52 +0100

    install firebase and add AuthContext

---
+++ src/firebase/config.ts
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBuMPFO73RwfxW3hY2sjpJT-zMHuCQBvUw",
+  authDomain: "garden-gnome-bab05.firebaseapp.com",
+  projectId: "garden-gnome-bab05",
+  storageBucket: "garden-gnome-bab05.firebasestorage.app",
+  messagingSenderId: "995454841160",
+  appId: "1:995454841160:web:8c4bf214b4aa0be0ae437d",
+  measurementId: "G-TY6NHZTZ61"
+};
+
+// Initialize Firebase
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app)
+const analytics = getAnalytics(app);
