a4b34cb463684b3cd05cd8fc17428a774fb86d89
kwidua <kwidua@gmail.com>
2026-03-21 16:08:53 +0100

    Add login screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import MyPlants from './pages/MyPlants'
 import Calendar from './pages/Calendar'
 import Todos from './pages/Todos'
+import { Login } from './pages/Login'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Route path="/my-plants" element={<MyPlants />} />
       <Route path="/calendar" element={<Calendar />} />
       <Route path="/todos" element={<Todos />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   )
 }
